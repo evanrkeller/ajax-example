2f6e32e0f79963febcc31186bed992b7849d08d5
Evan Keller <ekeller@barefootproximity.com>
2013-12-29 19:52:06 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeatherApi::Application.config.secret_key_base = '10969c00f06cf8729d66bd46ea8b8ab4866e88edd456aa418f9031a7c6c6fbc2621d58095a1fe999fcb6b42259f0f413274a1ab615c61f5d642cdfef51771314'
