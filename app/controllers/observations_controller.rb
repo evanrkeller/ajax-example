38a4b4cbddeeb7ea2968abc8b3120e9619f89b38
Evan Keller <ekeller@barefootproximity.com>
2013-12-29 23:03:35 -0600

    Bug Fix: set default query value

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -4,6 +4,7 @@ class ObservationsController < ApplicationController
   require 'json'
 
   def index
+    params[:q] = 'Birmingham, AL' if params[:q].blank?
     @observation =  JSON.parse(open("http://api.openweathermap.org/data/2.5/weather?q=#{URI::encode params[:q]}").read)
     respond_to do |format|
       format.html
